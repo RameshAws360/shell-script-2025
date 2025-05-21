cb4976314d185b3fb36141cc883367d8f4ccd9ee
ramesh <rameshaddicherla5@gmail.com>
2025-05-21 16:34:40 +0530

    special commit

--- o5-array.sh
+++ o5-array.sh
@@ -4,3 +4,10 @@ MOVIES=("COURT" "HIT3" "WAR")
 
 echo "first movie ${MOVIES[0]}"
 echo "first movie ${MOVIES[1]}"
+
+
+echo " number of variables passed $# "
+
+echo " All number variables $@ "
+
+echo " Script name is $0 "
